83c04d9603e99a71f34177d4454c4c7e8d9f725f
Kyle Lange <baronsintrees@gmail.com>
2017-04-06 16:50:41 -0700

    complete node response.on data and end functions to display for one user inputted at the bottom of the file.

--- treeTwo.js
+++ treeTwo.js
@@ -1,17 +1,37 @@
 const https = require('https');
-const username = 'kylelange';
+//const username = 'kylelange';
 
 function printMessage(username, badgeCount, points) {
-  const message = `${username} has ${badgeCount} total badge(s) and ${points} in JavaScript.`;
+  const message = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript.`;
   console.log(message);
 }
 
-const request = https.get(`https://teamtreehouse.com/${username}.json`, response  => {
-                console.log(response.statusCode);
-             });
+function getProfile(username) {
 
+const request = https.get(`https://teamtreehouse.com/${username}.json`, response  => {
+                let body = '';
+
+                response.on('data', data => {
+                  body += data.toString();
+                });
+
+                response.on('end', () => {
+                  const profile = JSON.parse(body);
+                  printMessage(username, profile.badges.length, profile.points.JavaScript);
+                });
+
+             });
+}
+
+getProfile('kylelange');
 // Steps
 //   1. require 'https'
 //   2. write a printMessage function with parameters you want in your string output.
 //   3. in printMessage: const message + console.log(); to see if it works.
 //   4. write an API request: const request =https.get(url+callback: response => {console.dir()});
+//5. let body = '';
+//6. response.on('data', data => {body += data.toString();})
+//every data event in node has a end event you can work with
+//* console.log(typeof body) will tell you what type of data you are recieving from your API: srting or object, for ex.
+//7. response.on('end', () => {console.log(body);});
+//8. Parse the string to turn it into a JSON object in the 'end' response= const profile = JSON.parse(body); then call printMessage(username,profile.badges.length,profile.etc...)
