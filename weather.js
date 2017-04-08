03e330f33809ced4699a50f088abe5d87919adee
Kyle Lange <baronsintrees@gmail.com>
2017-04-07 21:36:28 -0700

    use process.argv to input zipcodes via the console and return the correct data.

--- weather.js
+++ weather.js
@@ -2,26 +2,36 @@ const http = require('http');
 const https = require('https');
 const api = require('./api.json');
 
-const zipCode = '90210';
-//const weatherType = 'sunny';
-//const temp = 80;
-//const city = Beverly Hills;
+//const zipCode = '97201';
+
+//seed printWeather data
+//const weatherType = 'sunny, (I wish...)';
+//const temp = 60;
+//const city = Portland;
 
 function printWeather (city, weatherType, temp) {
   const message = `The weather in ${city} is ${weatherType} and ${temp} degrees F.`
    console.log(message);
 };
 
-const request = http.get(`http://api.wunderground.com/api/663c1433467f5b7f/conditions/q/${zipCode}.json`, response => {
+function getWeather(zipCode) {
+  const request = http.get(`http://api.wunderground.com/api/663c1433467f5b7f/conditions/q/${zipCode}.json`, response => {
 
-  let body = '';
+    let body = '';
 
-  response.on ('data', data => {
-    body += data.toString();
+    response.on ('data', data => {
+      body += data.toString();
+    });
+
+    response.on ('end', () => {
+      const place = JSON.parse(body);
+      printWeather( place.current_observation.display_location.full, place.current_observation.weather.toLowerCase(), place.current_observation.temp_f);
+    })
   });
+}
 
-  response.on ('end', () => {
-    const place = JSON.parse(body);
-    printWeather( place.current_observation.display_location.full, place.current_observation.weather.toLowerCase(), place.current_observation.temp_f);
-  })
-});
+
+const zipCodes = process.argv.slice(2)
+zipCodes.forEach(getWeather);
+
+//const ??? = process.argv
